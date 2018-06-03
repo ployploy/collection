495afa2b7c833a35f24689c7504aaa2dea0e2409
ploy <chonnikan.t@kkumail.com>
2018-06-03 22:02:24 +0700

    add comment for understand using

--- chapter7-7/main.go
+++ chapter7-7/main.go
@@ -13,8 +13,10 @@ func main() {
 	if mymap[3] != 0 {
 		fmt.Println(mymap[3])
 	}
-
-	//ok
+	// check ให้เห็นว่าจริงๆ  2 มันมีค่า defualt เลยเป็น true
+	// value, ok := mymap[2]
+	// fmt.Println(value, ok)
+	//ok ค่าตรงนี้มันเป็น defualt = 0 เพราะ 3 ไม่ได้ถูกกำหนดไว้
 	if value, ok := mymap[3]; ok {
 		fmt.Println(value)
 	}
